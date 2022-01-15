efcec60f47fd96b623d73b43b8b31af52b2f8120
Philipp Lackner <mail@pl-coding.com>
2022-01-15 18:43:30 +0100

    Part 23 - TrackableFoodItem

--- tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/search/SearchScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/search/SearchScreen.kt
@@ -1,23 +1,33 @@
 package com.plcoding.tracker_presentation.search
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.annotation.ExperimentalCoilApi
 import com.plcoding.core.util.UiEvent
 import com.plcoding.core_ui.LocalSpacing
+import com.plcoding.tracker_domain.model.MealType
 import com.plcoding.tracker_presentation.R
 import com.plcoding.tracker_presentation.search.components.SearchTextField
+import com.plcoding.tracker_presentation.search.components.TrackableFoodItem
 import kotlinx.coroutines.flow.collect
+import java.time.LocalDate
 
+@ExperimentalCoilApi
 @ExperimentalComposeUiApi
 @Composable
 fun SearchScreen(
@@ -69,5 +79,46 @@ fun SearchScreen(
                 viewModel.onEvent(SearchEvent.OnSearchFocusChange(it.isFocused))
             }
         )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.trackableFood) { food ->
+                TrackableFoodItem(
+                    trackableFoodUiState = food,
+                    onClick = {
+                        viewModel.onEvent(SearchEvent.OnToggleTrackableFood(food.food))
+                    },
+                    onAmountChange = {
+                        viewModel.onEvent(SearchEvent.OnAmountForFoodChange(
+                            food.food, it
+                        ))
+                    },
+                    onTrack = {
+                        viewModel.onEvent(
+                            SearchEvent.OnTrackFoodClick(
+                                food = food.food,
+                                mealType = MealType.fromString(mealName),
+                                date = LocalDate.of(year, month, dayOfMonth)
+                            )
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        when {
+            state.isSearching -> CircularProgressIndicator()
+            state.trackableFood.isEmpty() -> {
+                Text(
+                    text = stringResource(id = R.string.no_results),
+                    style = MaterialTheme.typography.body1,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
---
+++ tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/search/components/TrackableFoodItem.kt
@@ -0,0 +1,186 @@
+package com.plcoding.tracker_presentation.search.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.plcoding.core_ui.LocalSpacing
+import com.plcoding.tracker_presentation.R
+import com.plcoding.tracker_presentation.search.TrackableFoodUiState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import com.plcoding.tracker_presentation.components.NutrientInfo
+
+@ExperimentalCoilApi
+@Composable
+fun TrackableFoodItem(
+    trackableFoodUiState: TrackableFoodUiState,
+    onClick: () -> Unit,
+    onAmountChange: (String) -> Unit,
+    onTrack: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val food = trackableFoodUiState.food
+    val spacing = LocalSpacing.current
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(5.dp))
+            .padding(spacing.spaceExtraSmall)
+            .shadow(
+                elevation = 1.dp,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .background(MaterialTheme.colors.surface)
+            .clickable { onClick() }
+            .padding(end = spacing.spaceMedium)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                modifier = Modifier.weight(1f)
+            ) {
+                Image(
+                    painter = rememberImagePainter(
+                        data = food.imageUrl,
+                        builder = {
+                            crossfade(true)
+                            error(R.drawable.ic_burger)
+                            fallback(R.drawable.ic_burger)
+                        }
+                    ),
+                    contentDescription = food.name,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(topStart = 5.dp))
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                Column(
+                    modifier = Modifier.align(CenterVertically)
+                ) {
+                    Text(
+                        text = food.name,
+                        style = MaterialTheme.typography.body1,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Spacer(modifier = Modifier.height(spacing.spaceSmall))
+                    Text(
+                        text = stringResource(
+                            id = R.string.kcal_per_100g,
+                            food.caloriesPer100g
+                        ),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
+            Row {
+                NutrientInfo(
+                    name = stringResource(id = R.string.carbs),
+                    amount = food.carbsPer100g,
+                    unit = stringResource(id = R.string.grams),
+                    amountTextSize = 16.sp,
+                    unitTextSize = 12.sp,
+                    nameTextStyle = MaterialTheme.typography.body2
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                NutrientInfo(
+                    name = stringResource(id = R.string.protein),
+                    amount = food.proteinPer100g,
+                    unit = stringResource(id = R.string.grams),
+                    amountTextSize = 16.sp,
+                    unitTextSize = 12.sp,
+                    nameTextStyle = MaterialTheme.typography.body2
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                NutrientInfo(
+                    name = stringResource(id = R.string.fat),
+                    amount = food.fatPer100g,
+                    unit = stringResource(id = R.string.grams),
+                    amountTextSize = 16.sp,
+                    unitTextSize = 12.sp,
+                    nameTextStyle = MaterialTheme.typography.body2
+                )
+            }
+        }
+        AnimatedVisibility(visible = trackableFoodUiState.isExpanded) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(spacing.spaceMedium),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row {
+                    BasicTextField(
+                        value = trackableFoodUiState.amount,
+                        onValueChange = onAmountChange,
+                        keyboardOptions = KeyboardOptions(
+                            imeAction = if(trackableFoodUiState.amount.isNotBlank()) {
+                                ImeAction.Done
+                            } else ImeAction.Default
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = {
+                                onTrack()
+                                defaultKeyboardAction(ImeAction.Done)
+                            }
+                        ),
+                        singleLine = true,
+                        modifier = Modifier
+                            .border(
+                                shape = RoundedCornerShape(5.dp),
+                                width = 0.5.dp,
+                                color = MaterialTheme.colors.onSurface
+                            )
+                            .alignBy(LastBaseline)
+                            .padding(spacing.spaceMedium)
+                    )
+                    Spacer(modifier = Modifier.width(spacing.spaceExtraSmall))
+                    Text(
+                        text = stringResource(id = R.string.grams),
+                        style = MaterialTheme.typography.body1,
+                        modifier = Modifier.alignBy(LastBaseline)
+                    )
+                }
+                IconButton(
+                    onClick = onTrack,
+                    enabled = trackableFoodUiState.amount.isNotBlank()
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = stringResource(id = R.string.track)
+                    )
+                }
+            }
+        }
+    }
+}
