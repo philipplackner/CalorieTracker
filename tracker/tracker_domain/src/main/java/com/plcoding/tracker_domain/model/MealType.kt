38335eecd0d3fcca35cfd5f6df9108a0dc3107ec
Daniel Naranjo <98441185+kilosculptor@users.noreply.github.com>
2022-09-12 16:16:25 -0400

    Bug Fix - Meals defaulting to Breakfast list

--- tracker/tracker_domain/src/main/java/com/plcoding/tracker_domain/model/MealType.kt
+++ tracker/tracker_domain/src/main/java/com/plcoding/tracker_domain/model/MealType.kt
@@ -1,18 +1,18 @@
 package com.plcoding.tracker_domain.model
 
 sealed class MealType(val name: String) {
-    object Breakfast: MealType("breakfast")
-    object Lunch: MealType("lunch")
-    object Dinner: MealType("dinner")
-    object Snack: MealType("snack")
+    object Breakfast: MealType("Breakfast")
+    object Lunch: MealType("Lunch")
+    object Dinner: MealType("Dinner")
+    object Snack: MealType("Snacks")
 
     companion object {
         fun fromString(name: String): MealType {
             return when(name) {
-                "breakfast" -> Breakfast
-                "lunch" -> Lunch
-                "dinner" -> Dinner
-                "snack" -> Snack
+                "Breakfast" -> Breakfast
+                "Lunch" -> Lunch
+                "Dinner" -> Dinner
+                "Snacks" -> Snack
                 else -> Breakfast
             }
         }
