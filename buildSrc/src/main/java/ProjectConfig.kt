45c67d690b55fc0913d1f55e7e95e686bbe47146
Philipp Lackner <mail@pl-coding.com>
2023-03-20 10:17:01 +0100

    -Upgrading dependencies, AGP version, Kotlin version -Removing jcenter() repository -Upgrading targetSdk and compilerSdk to 33

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,8 +1,8 @@
 object ProjectConfig {
     const val appId = "com.plcoding.calorytracker"
-    const val compileSdk = 31
+    const val compileSdk = 33
     const val minSdk = 21
-    const val targetSdk = 31
+    const val targetSdk = 33
     const val versionCode = 1
     const val versionName = "1.0"
 }
