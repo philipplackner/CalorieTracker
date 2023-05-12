38b49cba247c5868c09d0020ec4b9ca3d2d93e23
Philipp Lackner <mail@pl-coding.com>
2023-05-12 16:17:48 +0200

    Updating Gradle to 8.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 android {
+    namespace = "com.plcoding.calorytracker"
     compileSdk = ProjectConfig.compileSdk
 
     defaultConfig {
--- buildSrc/src/main/java/Build.kt
+++ buildSrc/src/main/java/Build.kt
@@ -1,5 +1,5 @@
 object Build {
-    private const val androidBuildToolsVersion = "7.3.0"
+    private const val androidBuildToolsVersion = "8.0.1"
     const val androidBuildTools = "com.android.tools.build:gradle:$androidBuildToolsVersion"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
