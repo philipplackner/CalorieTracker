990a2fab9730fb38f58fb3197f970d2c7d7fc93d
Philipp Lackner <mail@pl-coding.com>
2023-08-24 19:34:32 +0200

    Module setup Gradle Kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "CaloryTracker"
 include(":app")
- 
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":tracker")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
+include(":tracker:tracker_presentation")
