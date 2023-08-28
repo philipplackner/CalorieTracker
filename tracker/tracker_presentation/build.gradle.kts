a4592eff5ba0be7d6e7d99635458616fcac79860
Philipp Lackner <mail@pl-coding.com>
2023-08-28 17:03:16 +0200

    Fixing string error

--- tracker/tracker_presentation/build.gradle.kts
+++ tracker/tracker_presentation/build.gradle.kts
@@ -11,6 +11,7 @@ android {
 
 dependencies {
     implementation(project(Modules.core))
+    implementation(project(Modules.coreUi))
     implementation(project(Modules.trackerDomain))
 
     implementation(Coil.coilCompose)
