ec4a7b90f358d5e20723ee2ba1807a2c23bcd0ae
Philipp Lackner <mail@pl-coding.com>
2023-08-28 16:52:20 +0200

    Fixing missing dependency error

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
     implementation("com.android.tools.build:gradle:8.0.2")
+
+    // Required to fix a missing dependency error
+    implementation("com.squareup:javapoet:1.13.0")
 }
 
 
